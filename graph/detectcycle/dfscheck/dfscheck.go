c1fe18c2ae4a0918ab2f8605976fab56eb2cf8a4
jwchae <jwchae@laonroad.com>
2024-03-05 09:27:43 +0900

    Feat: graph 관련 예제 코드 추가(cycle detection)

---
+++ graph/detectcycle/dfscheck/dfscheck.go
@@ -0,0 +1,42 @@
+package dfscheck
+
+type Node[T any] struct {
+	Value T
+	Links []*Node[T]
+}
+
+func (n *Node[T]) LinkNode(l *Node[T]) {
+	n.Links = append(n.Links, l)
+}
+
+func (n *Node[T]) Link(v T) *Node[T] {
+	newNode := &Node[T]{
+		Value: v,
+	}
+	n.Links = append(n.Links, newNode)
+	return newNode
+}
+
+func DetectLoop[T any](head *Node[T]) bool {
+	ancestors := make(map[*Node[T]]bool)
+	ancestors[head] = true
+
+	return internalDetectionLoop(head, ancestors)
+}
+
+func internalDetectionLoop[T any](node *Node[T], ancestors map[*Node[T]]bool) bool {
+	ancestors[node] = true
+	for _, l := range node.Links {
+		if ancestors[l] {
+			return true
+		}
+
+		if internalDetectionLoop[T](l, ancestors) {
+			return true
+		}
+
+	}
+
+	delete(ancestors, node)
+	return false
+}
